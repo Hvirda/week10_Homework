4c140ebb515d2e4e122a7bb34f79f33f1207c935
Hvirda <virda.hiral@gmail.com>
2023-05-29 17:29:47 +0100

    Project4

--- SauceDemo/src/baseFactory/BaseTest.java
+++ SauceDemo/src/baseFactory/BaseTest.java
@@ -1,2 +1,19 @@
-package baseFactory;public class BaseTest {
+package baseFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    public static WebDriver driver;
+
+    public void openBrowser(String url){
+        driver=new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+
+    }
+    public void closeBrowser(){
+        driver.quit();
+    }
+
 }
--- SauceDemo/src/testSuite/LoginTest.java
+++ SauceDemo/src/testSuite/LoginTest.java
@@ -1,2 +1,42 @@
-package testSuite;public class LoginTest {
+package testSuite;
+
+import baseFactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class LoginTest extends BaseTest {
+
+    String baseUrl="https://www.saucedemo.com/";
+    @Before
+    public void setup(){
+        openBrowser(baseUrl);
+    }
+    @Test
+    public void test1(){
+        WebElement uname=driver.findElement(By.xpath("//input[@id='user-name']"));
+        uname.sendKeys("standard_user");
+        WebElement pwd=driver.findElement(By.xpath("//input[@id='password']"));
+        pwd.sendKeys("secret_sauce");
+        WebElement btn=driver.findElement(By.xpath("//input[@id='login-button']"));
+        btn.click();
+
+        WebElement product=driver.findElement(By.xpath("//div/span[text()='Products']"));
+        String actualProduct=product.getText();
+        String expectedProduct="Products";
+        Assert.assertEquals("verify product",expectedProduct,actualProduct);
+
+       List<WebElement> item= driver.findElements(By.xpath("//div[@class='inventory_item']"));
+        System.out.println(item.size());
+
+    }
+    @After
+    public void tearDown(){
+        closeBrowser();
+    }
 }
